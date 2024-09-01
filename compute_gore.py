291ce389b314ad0bda3509f985d439788d124772
Bella <annabel.jakob@hotmail.de>
2024-09-01 13:23:35 +0100

    add method to compute gore height

--- compute_gore.py
+++ compute_gore.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import pi
 
 MIN_RADIUS = 0.0
 MAX_RADIUS = 90.0
@@ -6,3 +7,6 @@ MAX_RADIUS = 90.0
 def compute_radii(step_size : float = 1.0):
 
     return np.arange(MIN_RADIUS, MAX_RADIUS+step_size, step_size)[::-1]
+
+def compute_height(radius: float):
+    return (2.0*pi*radius)/4.0
---
+++ test/compute_gore_compute_height_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from compute_gore import compute_height
+
+class ComputeHeightTestCase(unittest.TestCase):
+
+    def test_with_radius_1(self):
+        actual_height = compute_height(1.0)
+        expected_height = 1.570796327
+        self.assertAlmostEqual(expected_height, actual_height)
+
+    def test_with_radius_10(self):
+        actual_height = compute_height(10.0)
+        expected_height = 15.70796327
+        self.assertAlmostEqual(expected_height, actual_height)
