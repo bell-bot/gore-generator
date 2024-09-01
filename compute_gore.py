226ab35ef6902138868de76d220b86e456e75b38
Bella <annabel.jakob@hotmail.de>
2024-09-01 20:58:58 +0100

    rename compute_radii method to compute_step_angles

--- compute_gore.py
+++ compute_gore.py
@@ -4,7 +4,7 @@ from math import pi, sin, radians
 MIN_RADIUS = 0.0
 MAX_RADIUS = 90.0
 
-def compute_radii(step_size : float = 1.0):
+def compute_step_angles(step_size : float = 1.0):
     return np.arange(MIN_RADIUS, MAX_RADIUS+step_size, step_size)[::-1]
 
 def compute_height(radius: float):
@@ -16,3 +16,13 @@ def compute_step(radius: float, height: float, angle: float, n_gores: int):
     w_angle = (2*pi*r_angle)/n_gores
     
     return np.array([h_angle, w_angle])
+
+def compute_gore_dimensions(radii: np.ndarray, height, n_gores):
+    dimensions = []
+    for radius in radii.tolist():
+        dimensions.append(compute_step(radius, height, ))
+
+if __name__=="__main__":
+    r = compute_step_angles()
+    print(r.shape)
+    h = compute_gore_dimensions(r, 10, 6)
--- test/compute_gore_compute_radii_test.py
+++ test/compute_gore_compute_radii_test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from compute_gore import compute_radii
+from compute_gore import compute_step_angles
 
-class ComputeRadiiTestCase(unittest.TestCase):
+class ComputeStepAnglesTestCase(unittest.TestCase):
     
     def test_with_default_step_size(self):
-        actual_radii = compute_radii()
+        actual_radii = compute_step_angles()
         expected_length = 91
         expected_start_value = 90.0
         expected_stop_value = 0.0
@@ -15,7 +15,7 @@ class ComputeRadiiTestCase(unittest.TestCase):
         self.assertEqual(expected_stop_value, actual_radii[-1])
 
     def test_with_step_size_0_5(self):
-        actual_radii = compute_radii(step_size=0.5)
+        actual_radii = compute_step_angles(step_size=0.5)
         expected_length = 181
         expected_start_value = 90.0
         expected_stop_value = 0.0
@@ -25,7 +25,7 @@ class ComputeRadiiTestCase(unittest.TestCase):
         self.assertEqual(expected_stop_value, actual_radii[-1])
 
     def test_with_step_size_2_5(self):
-        actual_radii = compute_radii(step_size=2.5)
+        actual_radii = compute_step_angles(step_size=2.5)
         expected_length = 37
         expected_start_value = 90.0
         expected_stop_value = 0.0
