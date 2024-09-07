ceb1c1368270ee206661ca942bccb74b5d7681ff
Bella <annabel.jakob@hotmail.de>
2024-09-07 01:07:01 +0100

    use numpy array functions to compute dimensions instead of looping through array

--- compute_gore.py
+++ compute_gore.py
@@ -10,19 +10,32 @@ def compute_step_angles(step_size : float = 1.0):
 def compute_height(radius: float):
     return (2.0*pi*radius)/4.0
 
+def compute_sin(angles: np.ndarray):
+    result_array = np.tile(angles, (2,1))
+    result_array[1] = np.sin(np.deg2rad(result_array[1]))
+    return result_array
+
 def compute_step(radius: float, height: float, angle: float, n_gores: int):
     r_angle = sin(radians(angle))*radius
     h_angle = height*((90.0-angle)/90.0)
     w_angle = (2*pi*r_angle)/n_gores
     
-    return np.array([h_angle, w_angle])
+    return [h_angle, w_angle]
 
-def compute_gore_dimensions(radii: np.ndarray, height, n_gores):
-    dimensions = []
-    for radius in radii.tolist():
-        dimensions.append(compute_step(radius, height, ))
+def compute_gore_dimensions(input_array: np.ndarray, height: float, n_gores: int, height_const: float, width_const: float):
+    
+    const_array = np.array([height_const, width_const]).reshape((2,1))
+    input_array[0] = 90 - input_array[0]
+        
+    return np.multiply(const_array,input_array)
+
+def get_height_constant(height: float):
+    return height/90.0
+
+def get_width_constant(n_gores: int):
+    return (2*pi)/n_gores
 
 if __name__=="__main__":
     r = compute_step_angles()
-    print(r.shape)
-    h = compute_gore_dimensions(r, 10, 6)
+    s = compute_sin(r)
+    print(s.shape)
---
+++ test/compute_gore_compute_gore_dimensions_test.py
@@ -0,0 +1,24 @@
+from unittest.mock import MagicMock
+import unittest
+import numpy as np
+import numpy.testing as nptest
+
+from compute_gore import compute_gore_dimensions
+
+
+class ComputeGoreDimensionsTestCase(unittest.TestCase):
+    
+    def test_returns_correct_dimensions(self):
+        input_array = np.array([[0.0,30.0,90.0],[0, 0.5, 1]], dtype=np.float64)
+        height = 1.0
+        n_gores = 6
+        height_const = 0.5
+        width_const = 0.1
+        
+        actual_result = compute_gore_dimensions(input_array, height, n_gores, height_const, width_const)
+        
+        expected_result = np.array([[45.0, 30.0, 0.0],[0.0,0.05,0.1]], dtype=np.float64)
+        
+        nptest.assert_allclose(actual_result, expected_result)
+        
+        
