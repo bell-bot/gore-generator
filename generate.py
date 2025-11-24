f6871fec776e6c70abe60f428c22afc0b7b42fe1
bella <annabel.jakob@hotmail.de>
2025-11-24 15:50:09 +0000

    Add uuid to distinguish tmp files

--- generate.py
+++ generate.py
@@ -19,7 +19,7 @@ def generate(radius: float, n_gores: int, precision: float, uuid: str):
     gore = get_gore(radius, n_gores, step_size)
     
     fig, ax = plot_gore(gore)
-    save_outputs(fig,ax)
+    save_outputs(fig,ax, uuid)
 
     return True
 
