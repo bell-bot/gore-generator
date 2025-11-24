7a4216cd4b3e1768635d2a3dd9f453c6fa8f0342
bella <annabel.jakob@hotmail.de>
2025-11-24 15:18:50 +0000

    change tmp path

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import os
 
-os.makedirs('tmp', exist_ok=True)
+os.makedirs('/tmp', exist_ok=True)
 
-PDF_PATH = "tmp/tmp.pdf"
-PNG_PATH = "tmp/tmp.png"
+PDF_PATH = "/tmp/tmp.pdf"
+PNG_PATH = "/tmp/tmp.png"
 ZIP_PATH = "tmp/tmp.zip"
