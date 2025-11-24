3d84714b1942b61b1f2e010b96876560e413d8cb
bella <annabel.jakob@hotmail.de>
2025-11-24 15:29:27 +0000

    fix zip path

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ os.makedirs('/tmp', exist_ok=True)
 
 PDF_PATH = "/tmp/tmp.pdf"
 PNG_PATH = "/tmp/tmp.png"
-ZIP_PATH = "tmp/tmp.zip"
+ZIP_PATH = "/tmp/tmp.zip"
--- util.py
+++ util.py
@@ -4,7 +4,7 @@ from constants import PDF_PATH, PNG_PATH, ZIP_PATH
 
 
 def zip_response():
-    zipped = zipfile.ZipFile( "/" + ZIP_PATH, 'w', zipfile.ZIP_DEFLATED)
+    zipped = zipfile.ZipFile( ZIP_PATH, 'w', zipfile.ZIP_DEFLATED)
     print(f"Created ZIP file at {zipped.pwd}")
     zipped.write(PDF_PATH)
     zipped.write(PNG_PATH)
