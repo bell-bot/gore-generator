d971174b7c146e12dfb8de78b3843bf74a9cbf19
bella <annabel.jakob@hotmail.de>
2025-11-24 15:06:47 +0000

    Add logging to file saving workflow

--- generate.py
+++ generate.py
@@ -7,6 +7,11 @@ from plot_gore import plot_gore
 
 from compute_gore import get_gore
 
+import os
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='generate.log', encoding='utf-8', level=logging.DEBUG)
+
 def get_step_size_from_precision(precision: float):
     return 50.0/precision
 
@@ -23,6 +28,8 @@ def generate(radius: float, n_gores: int, precision: float):
 
 def save_outputs(fig: Figure, ax: Axes):
     fig.savefig(PDF_PATH, format='pdf')
+    logger.debug(f"Saved PDF to {os.path.abspath(PDF_PATH)}")
+
     aspect_ratio = ax.get_aspect()
 
     if type(aspect_ratio) != float:
@@ -30,3 +37,4 @@ def save_outputs(fig: Figure, ax: Axes):
 
     fig.set_size_inches(800/fig.dpi, (800*aspect_ratio)/fig.dpi)
     fig.savefig(PNG_PATH, format='png', bbox_inches="tight")
+    logger.debug(f"Saved PNG to {os.path.abspath(PNG_PATH)}")
--- util.py
+++ util.py
@@ -2,8 +2,14 @@ import zipfile
 
 from constants import PDF_PATH, PNG_PATH, ZIP_PATH
 
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='utils.log', encoding='utf-8', level=logging.DEBUG)
+
 def zip_response():
     zipped = zipfile.ZipFile( ZIP_PATH, 'w', zipfile.ZIP_DEFLATED)
+    logger.debug(f"Created ZIP file at {zipped.pwd}")
     zipped.write(PDF_PATH)
     zipped.write(PNG_PATH)
 
